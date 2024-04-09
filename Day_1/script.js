f333885dfaf64875c20d1d54584f1d085b4fe0c6
Shivani <165997406+Vanicoding27@users.noreply.github.com>
2024-04-09 11:40:57 +0000

    updated

---
+++ Day_1/script.js
@@ -0,0 +1,21 @@
+let hour = document.querySelector(".hour_hand");
+let minute = document.querySelector(".minute_hand");
+let second = document.querySelector(".sec_hand");
+
+function Displaytime() {
+    const data = new Date();
+    let hh = data.getHours();
+    let mm = data.getMinutes();
+    let ss = data.getSeconds();
+
+    let hhrotation = 30 * hh + 2 / mm + ss / 60;
+    let mmrotation = 6 * mm + ss / 10;
+    let ssrotation = 6 * ss;
+
+    hour.style.transform = `rotate(${hhrotation}deg)`;
+    minute.style.transform = `rotate(${mmrotation}deg)`;
+    second.style.transform = `rotate(${ssrotation}deg)`;
+}
+
+// Call the function every second
+setInterval(Displaytime, 1000);
